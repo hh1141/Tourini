b901b3d30cf8f956476bfa4752c90422ce7ca184
hong7463 <hison7463@gmail.com>
2015-04-27 12:00:04 -0400

    Add devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show]
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end 
+
+  def new
+    @post = current_user.posts.build
+  end 
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "Successfully create post"
+    else 
+      render 'new'
+    end 
+  end 
+
+  def edit
+  end 
+
+  def update
+  end 
+
+  def destroy
+  end 
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text, :photo, :location)
+  end 
+
+  def find_post
+    @post = Post.find(params[:id])
+  end 
+end
